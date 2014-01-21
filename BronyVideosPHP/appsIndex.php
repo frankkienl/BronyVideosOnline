364fbbd5f885b3be81e59be166c2628df13e20da
Frank Bouwens <frankkie12345@gmail.com>
2014-01-21 22:56:23 +0100

    Added appsIndex

---
+++ BronyVideosPHP/appsIndex.php
@@ -0,0 +1,50 @@
+
+<h2>Pony Stuff</h2>
+
+<table border=1>
+<tr><th>What<th>Android<th>HTML5<th>Open Source<th>Details</tr>
+
+<tr>
+<td>Muffin Game
+<td><a href="frikandel-gdx-android.apk">APK</a>
+<td><a href="muffingame/">Online</a>
+<td>Nope.
+<td>Catch the muffins game.<br />Made with LibGDX
+</tr>
+
+<tr>
+<td>Brony Livewallpaper
+<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronylivewallpaper">Google Play</a>
+<td><a href="livewallpaper/">Online</a>
+<td><a href="https://github.com/frankkienl/BronyLiveWallpaper">Android</a><br />
+<a href="https://github.com/frankkienl/BronyLivewallpaperOnline">Online</a>
+<td>See some ponies running around, based on Desktop Ponies<br />Native Android / JavaScript (Cordova)<br /><a href="http://www.equestriadaily.com/2013/09/nightly-roundup-744.html">EQD</a>
+</tr>
+
+<tr>
+<td>Brony MLP Blindbag Guide
+<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronymlpblindbagguide">Google Play</a>
+<td><a href="http://frankkie.nl/pony/blindbag/web/">Online</a>
+<td><a href="https://github.com/frankkienl/BronyMLPBlindBagGuide">Android</a><br />
+<a href="https://github.com/frankkienl/BronyMLPBlindBagGuideOnline">Online</a>
+<td>Native Android / Dart (JavaScript)<br /><a href="http://www.equestriadaily.com/2013/08/android-brony-mlp-blind-bag-guide.html">EQD</a>
+</tr>
+
+<tr>
+<td>Brony MLP Quote
+<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronyquotes">Google Play</a>
+<td><a href="quotes/">Online</a>
+<td><a href="https://github.com/frankkienl/BronyQuotes">Android</a>
+<td>Soundboard. Sounds are taken from Desktop Ponies<br />Native Android
+</tr>
+
+<tr>
+<td>Brony MLP Videos
+<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronyvideos">Google Play</a>
+<td><a href="videos/">online</a>
+<td><a href="https://github.com/frankkienl/BronyVideos">Android</a><br />
+<a href="https://github.com/frankkienl/BronyVideosOnline">Online</a>
+<td>Check videos from MLP Episodes, videos are on Dailymotion.<br />Native Android / PHP
+</tr>
+
+</table>
