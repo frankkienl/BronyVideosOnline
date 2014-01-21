d399c7726a6336e27f0d172c369b413160530dbb
Frank Bouwens <frankkie12345@gmail.com>
2014-01-21 23:26:24 +0100

    modified appsIndex

--- BronyVideosPHP/appsIndex.php
+++ BronyVideosPHP/appsIndex.php
@@ -1,50 +1,112 @@
+<html>
+    <head>
+        <title>Brony Apps</title>
+        <style type="text/css">            
+            body {background-color: lightskyblue; font-family: sans-serif}
+            .app_button {float:left; margin: 10px}
+        </style>
+    </head>
+    <body>
+        <h2>Brony Apps</h2>
+        <div id="div_buttons_grid">
 
-<h2>Pony Stuff</h2>
+            <div class="app_button" id="button_livewallpaper">
+                <a href="livewallpaper/main.html">
+                    <center>
+                        <img width="96" border="0" src="icon_livewallpaper.png"/><br />
+                        Live Wallpaper
+                    </center>
+                </a>
+            </div>
 
-<table border=1>
-<tr><th>What<th>Android<th>HTML5<th>Open Source<th>Details</tr>
+            <div class="app_button" id="button_blindbag">
+                <a href="blindbag/web/">
+                    <center>
+                        <img width="96" border="0" src="icon_blindbag.png"/><br />
+                        Blind Bag Guide
+                    </center>
+                </a>
+            </div>
 
-<tr>
-<td>Muffin Game
-<td><a href="frikandel-gdx-android.apk">APK</a>
-<td><a href="muffingame/">Online</a>
-<td>Nope.
-<td>Catch the muffins game.<br />Made with LibGDX
-</tr>
+            <div class="app_button" id="button_quotes">
+                <a href="quotes/">
+                    <center>
+                        <img width="96" border="0" src="icon_quotes.png"/><br />
+                        Quotes
+                    </center>
+                </a>
+            </div>
 
-<tr>
-<td>Brony Livewallpaper
-<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronylivewallpaper">Google Play</a>
-<td><a href="livewallpaper/">Online</a>
-<td><a href="https://github.com/frankkienl/BronyLiveWallpaper">Android</a><br />
-<a href="https://github.com/frankkienl/BronyLivewallpaperOnline">Online</a>
-<td>See some ponies running around, based on Desktop Ponies<br />Native Android / JavaScript (Cordova)<br /><a href="http://www.equestriadaily.com/2013/09/nightly-roundup-744.html">EQD</a>
-</tr>
+            <div class="app_button" id="button_videos">
+                <a href="videos/">
+                    <center>
+                        <img width="96" border="0" src="icon_videos.png"/><br />
+                        Videos
+                    </center>
+                </a>
+            </div>
 
-<tr>
-<td>Brony MLP Blindbag Guide
-<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronymlpblindbagguide">Google Play</a>
-<td><a href="http://frankkie.nl/pony/blindbag/web/">Online</a>
-<td><a href="https://github.com/frankkienl/BronyMLPBlindBagGuide">Android</a><br />
-<a href="https://github.com/frankkienl/BronyMLPBlindBagGuideOnline">Online</a>
-<td>Native Android / Dart (JavaScript)<br /><a href="http://www.equestriadaily.com/2013/08/android-brony-mlp-blind-bag-guide.html">EQD</a>
-</tr>
+            <div class="app_button" id="button_muffingame">
+                <a href="muffingame/">
+                    <center>
+                        <img width="96" border="0" src="icon_muffingame.png"/><br />
+                        Muffin Game
+                    </center>
+                </a>
+            </div>
 
-<tr>
-<td>Brony MLP Quote
-<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronyquotes">Google Play</a>
-<td><a href="quotes/">Online</a>
-<td><a href="https://github.com/frankkienl/BronyQuotes">Android</a>
-<td>Soundboard. Sounds are taken from Desktop Ponies<br />Native Android
-</tr>
+        </div>
+        <br />
+        <hr style="clear: both"/>
+        <br />
+        <table border=1>
+            <tr><th>What<th>Android<th>HTML5<th>Open Source<th>Details</tr>
 
-<tr>
-<td>Brony MLP Videos
-<td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronyvideos">Google Play</a>
-<td><a href="videos/">online</a>
-<td><a href="https://github.com/frankkienl/BronyVideos">Android</a><br />
-<a href="https://github.com/frankkienl/BronyVideosOnline">Online</a>
-<td>Check videos from MLP Episodes, videos are on Dailymotion.<br />Native Android / PHP
-</tr>
+            <tr>
+                <td>Muffin Game
+                <td><a href="frikandel-gdx-android.apk">APK</a>
+                <td><a href="muffingame/">Online</a>
+                <td>Nope.
+                <td>Catch the muffins game.<br />Made with LibGDX
+            </tr>
 
-</table>
+            <tr>
+                <td>Brony Livewallpaper
+                <td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronylivewallpaper">Google Play</a>
+                <td><a href="livewallpaper/">Online</a>
+                <td><a href="https://github.com/frankkienl/BronyLiveWallpaper">Android</a><br />
+                    <a href="https://github.com/frankkienl/BronyLivewallpaperOnline">Online</a>
+                <td>See some ponies running around, based on Desktop Ponies<br />Native Android / JavaScript (Cordova)<br /><a href="http://www.equestriadaily.com/2013/09/nightly-roundup-744.html">EQD</a>
+            </tr>
+
+            <tr>
+                <td>Brony MLP Blindbag Guide
+                <td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronymlpblindbagguide">Google Play</a>
+                <td><a href="http://frankkie.nl/pony/blindbag/web/">Online</a>
+                <td><a href="https://github.com/frankkienl/BronyMLPBlindBagGuide">Android</a><br />
+                    <a href="https://github.com/frankkienl/BronyMLPBlindBagGuideOnline">Online</a>
+                <td>Native Android / Dart (JavaScript)<br /><a href="http://www.equestriadaily.com/2013/08/android-brony-mlp-blind-bag-guide.html">EQD</a>
+            </tr>
+
+            <tr>
+                <td>Brony MLP Quote
+                <td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronyquotes">Google Play</a>
+                <td><a href="quotes/">Online</a>
+                <td><a href="https://github.com/frankkienl/BronyQuotes">Android</a><br />
+                    <a href="https://github.com/frankkienl/BronyVideosOnline">Online</a>
+                <td>Soundboard. Sounds are taken from Desktop Ponies<br />Native Android
+            </tr>
+
+            <tr>
+                <td>Brony MLP Videos
+                <td><a href="https://play.google.com/store/apps/details?id=nl.frankkie.bronyvideos">Google Play</a>
+                <td><a href="videos/">Online</a>
+                <td><a href="https://github.com/frankkienl/BronyVideos">Android</a><br />
+                    <a href="https://github.com/frankkienl/BronyVideosOnline">Online</a>
+                <td>Check videos from MLP Episodes, videos are on Dailymotion.<br />Native Android / PHP
+            </tr>
+
+        </table>
+
+    </body>
+</html>
